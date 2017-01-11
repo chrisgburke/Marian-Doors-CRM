5a778c1e2bf1fabe4c0db71b80793fc4367dd2f7
Chris Burke <chris@increase.co.uk>
2017-01-11 13:29:16 +0000

    Moved table script logic into Repository

--- CustomPages/TableScriptRepository.js
+++ CustomPages/TableScriptRepository.js
@@ -1,6 +1,6 @@
 //*
 //COMMON FUNCTIONS
-function LoggingOn(){
+function LoggingOn() {
     return true;
 }
 
@@ -12,9 +12,9 @@ function logFileName() {
 
 
 function writeToFile(message) {
-    
-    if(LoggingOn()){
-        try {  
+
+    if (LoggingOn()) {
+        try {
             var fso = new ActiveXObject("Scripting.FileSystemObject");
             var s = fso.OpenTextFile(logFileName(), 8, true);
             var fName = arguments.callee.caller.toString().match(/function ([^\(]+)/)[1]
@@ -23,45 +23,128 @@ function writeToFile(message) {
             s.writeline("");
             s.Close();
             fso = null;
-        }catch(error){
+        } catch (error) {
             ErrorStr += message;
         }
-        
+
     }
 }
-//COMMON FUNCTIONS
 
-function ENTITY_TSNAME_InsertRecord(){
+//*************************
+// Entity : QuoteItems 
+// Script : TaxCalculation 
+//*************************
+function QuoteItems_TaxCalculation_InsertRecord() {
     try {
-        
+        CalculateTax();
+        CalculateGrossValue();
     } catch (error) {
         writeToFile("ERROR: " + error.message);
     }
 }
 
-function ENTITY_TSNAME_PostInsertRecord(){
-	try {
-        
-    } catch (error) {
-        writeToFile("ERROR: " + error.message);
-    }
-}
-
-function ENTITY_TSNAME_UpdateRecord(){
-	try {
-        
-    } catch (error) {
-        writeToFile("ERROR: " + error.message);
-    }
-}
-
-function ENTITY_TSNAME_DeleteRecord(){
+function QuoteItems_TaxCalculation_PostInsertRecord() {
     try {
-        
+
     } catch (error) {
         writeToFile("ERROR: " + error.message);
     }
 }
+
+function QuoteItems_TaxCalculation_UpdateRecord() {
+    try {
+        CalculateTax();
+        CalculateGrossValue();
+    } catch (error) {
+        writeToFile("ERROR: " + error.message);
+    }
+}
+
+function QuoteItems_TaxCalculation_DeleteRecord() {
+    try {
+
+    } catch (error) {
+        writeToFile("ERROR: " + error.message);
+    }
+}
+
+function Calculate() {
+    var quotedpriceTotal = CalculateTax();
+    CalculateGrossValue(quotedPriceTotal);
+}
+
+function CalculateTax() {
+
+    var quotedPriceTotal = parseFloat(Values('Quit_quotedpricetotal')).toFixed(2);
+    writeToFile("Quoted price total = " + quotedPriceTotal);
+
+    var taxRate = parseFloat(Values('Quit_taxrate') / 100).toFixed(2);
+    writeToFile("Tax Rate = " + taxRate);
+
+    var taxcalc = parseFloat(quotedPriceTotal * taxRate).toFixed(2);
+    writeToFile("Tax Calc = " + taxcalc);
+
+    Values('Quit_linetaxvalue') = taxcalc;
+
+    return quotedPriceTotal;
+}
+
+function CalculateGrossValue(qpTotal) {
+    var lineTaxValue = parseFloat(Values('Quit_linetaxvalue')).toFixed(2);
+    writeToFile("Line Tax Value = " + lineTaxValue);
+    var grosscalc = parseFloat(qpTotal + lineTaxValue).toFixed(2);
+    writeToFile("Gross Calc = " + grosscalc);
+    Values('Quit_GrossValue') = grosscalc;
+}
+//******************************************************************************************************************************
+
+//*************************
+// Entity : OppoLink 
+// Script : UpdateNameField 
+//*************************
+function OppoLink_UpdateNameField_InsertRecord(){
+   try {
+       UpdateName();
+   } catch (error) {
+       writeToFile("ERROR: " + error.message);
+   }
+}
+
+function OppoLink_UpdateNameField_PostInsertRecord(){
+   try {
+
+   } catch (error) {
+       writeToFile("ERROR: " + error.message);
+   }
+}
+
+function OppoLink_UpdateNameField_UpdateRecord(){
+   try {
+       UpdateName();
+   } catch (error) {
+       writeToFile("ERROR: " + error.message);
+   }
+}
+
+function OppoLink_UpdateNameField_DeleteRecord(){
+   try {
+
+   } catch (error) {
+       writeToFile("ERROR: " + error.message);
+   }
+}
+
+function UpdateName(){
+    var nameField = 'Edit';
+    Values('OpLi_Name') = nameField;
+}
+
+//*********************************************************************************************/
+
+
+
+
+
 
 
 
